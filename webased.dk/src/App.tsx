8cb0e8a04ea337be01195149587484ecae5a727a
LAKAljs <ljs@laka.dk>
2023-04-24 12:11:10 +0200

    add webster api

--- webased.dk/src/App.tsx
+++ webased.dk/src/App.tsx
@@ -3,17 +3,34 @@ import logo from './logo.svg';
 import './App.css';
 import { Container } from '@mui/material';
 
-function App() {
-  return (
-    <div>
-      <Container style={{backgroundColor: "black"}}>
-        hej
-      </Container>
-      <Container style={{backgroundColor: "green"}} className='maxWidthLg'>
-        hej
-      </Container>
-    </div>
+import axios from "axios";
+import { useEffect, useState } from "react";
+import { stringify } from 'querystring';
 
+
+
+async function getSongs() {
+  // try {
+  const url = "https://dictionaryapi.com/api/v3/references/sd4/json/test?key=b61190ae-fde0-4a5d-9a77-b4d9bd65d8b7";
+  const options = {
+    method: 'GET',
+  }
+  await fetch(url, options)
+    .then(response => response.json())
+    .then(result => JSON.stringify(result))
+    .then(result => {});
+}
+
+function App() {
+    console.log(getSongs());
+
+  return (
+    
+    <div className="App">
+      <h1>Songs</h1>
+      <ul>
+      </ul>
+    </div>
   );
 }
 
